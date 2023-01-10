0dac826caf688033ba6a3bd506c1bcc3d2d0f90d
nenaddjordjevic <nenad.djordjevic@basiq.io>
2023-01-10 08:32:32 -0600

    q1 improved with docs and brute force solution

--- q1-two-sums/q1_two_sums.go
+++ q1-two-sums/q1_two_sums.go
@@ -1,13 +1,58 @@
 package q1_two_sums
 
 // leetcode #1
-func twoSums(nums []int, target int) []int {
-	m := make(map[int]int)
-	for i, num := range nums {
-		if idx, ok := m[target-num]; ok {
-			return []int{idx, i}
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//  1. Verify the constraints:
+//     All nums positive
+//     No duplicates in the array
+//     No they may not be always a solution, return nil
+//     You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//  2. Write test-cases (use unit-tests)
+//  3. Figure out solution without code
+//     numberToFind = target - nums[p1]
+//  4. Write brute force solution
+//  5. Double-check for errors
+//     Check for typos, closing our loops etc...
+//  6. Test with test cases manually walking through the code execution. Test edge cases too!
+//  7. Time and space complexity
+//  8. Optimize solution
+
+// Brute force: nested loops
+//
+//	nums = [1, 3, 7, 9, 2], target = 11
+//	nums = [1, 3, 7, 9, 2], target = 25
+//	nums = [], target = 11
+//	nums = [1], target = 11
+func twoSumsBruteForce(nums []int, target int) []int { // Time: O(n^2), Space: O(1)
+	for p1 := 0; p1 < len(nums); p1++ { // p1 = 0
+		numberToFind := target - nums[p1]        // numberToFind = 0
+		for p2 := p1 + 1; p2 < len(nums); p2++ { // p2 = 4
+			if numberToFind == nums[p2] {
+				return []int{p1, p2}
+			}
 		}
-		m[num] = i
+	}
+	return nil
+}
+
+// Optimized:
+//
+//	     iterate over an array (p as a pointer)
+//			check if there is a [target-num] item in a map
+//				yes: return idx, p
+//				no:  store in a map: num as a key, p as a value (numsMap[num] = p)
+//
+//		nums = [1, 3, 7, 9, 2], target = 11
+//		nums = [1, 3, 7, 9, 2], target = 25
+//		nums = [], target = 11
+//		nums = [1], target = 11
+func twoSums(nums []int, target int) []int { // Time: O(n), Space: O(n)
+	numsMap := make(map[int]int) // {1:0, 3:1, 7:2, 9:3}
+	for p, num := range nums {   // p=4, num=2
+		if idx, ok := numsMap[target-num]; ok {
+			return []int{idx, p} // [3, 4]
+		}
+		numsMap[num] = p
 	}
 	return nil
 }
