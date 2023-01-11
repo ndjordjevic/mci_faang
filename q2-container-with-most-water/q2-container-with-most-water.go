35619a6d513378b3031d6ea4fa2790ffce72fb91
nenaddjordjevic <nenad.djordjevic@basiq.io>
2023-01-11 09:07:29 -0600

    q2 brute force solution with tests

--- q2-container-with-most-water/q2-container-with-most-water.go
+++ q2-container-with-most-water/q2-container-with-most-water.go
@@ -3,6 +3,50 @@ package q2_container_with_most_water
 import "math"
 
 // leetcode #11
+// You are given an array of positive ints where each int represents the height of a vertical line on a chart
+// Find two lines which together with x-axis forms a container that would hold the grates amount of water.
+// Return the area that would hold
+// 1. Constraints:
+//		thickness of the line doesn't affect the area
+//		the sides can't be used to form a container
+//		lines inside a container don't affect the area, you can ignore them
+// 2. Test cases
+//		[7, 1, 2, 3, 9], 28
+// 		[], 0
+//		[7], 0
+//		[6, 9, 3, 4, 5, 8], 32
+// 3. Solution without code
+//		max question
+//		area = len * width; area = min(a, b) * (bi - ai)
+
+// Brute force: nested loops
+//
+//			maxArea = 0
+//			iterate over array (p1=0)
+//	     		iterate over array (p2=p1+1)
+//					check if min(heights[p1], heights[p2]) * (p2 - p1) > maxArea
+//						yes: set new maxArea
+//						no: continue
+//
+//		[7, 1, 2, 3, 9], 28
+//		[], 0
+//		[7], 0
+//		[6, 9, 3, 4, 5, 8], 32
+func maxWaterContainerBruteForce(heights []int) int { // Time: O(n^2), Space: O(1)
+	maxArea := 0                           // maxArea=32
+	for p1 := 0; p1 < len(heights); p1++ { // p1=1
+		for p2 := p1 + 1; p2 < len(heights); p2++ { // p2=5
+			height := int(math.Min(float64(heights[p1]), float64(heights[p2]))) // height=8
+			width := p2 - p1                                                    // width=4
+			area := height * width                                              // area=32
+			if area > maxArea {
+				maxArea = area
+			}
+		}
+	}
+	return maxArea
+}
+
 func maxWaterContainer(heights []int) int {
 	p1, p2, maxArea := 0, len(heights)-1, 0
 
--- q2-container-with-most-water/q2-container-with-most-water_test.go
+++ q2-container-with-most-water/q2-container-with-most-water_test.go
@@ -2,6 +2,53 @@ package q2_container_with_most_water
 
 import "testing"
 
+func Test_maxWaterContainerBruteForce(t *testing.T) {
+	type args struct {
+		heights []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Should return 28",
+			args: args{
+				[]int{7, 1, 2, 3, 9},
+			},
+			want: 28,
+		},
+		{
+			name: "Should return 0 empty array",
+			args: args{
+				[]int{},
+			},
+			want: 0,
+		},
+		{
+			name: "Should return 0 one el only",
+			args: args{
+				[]int{7},
+			},
+			want: 0,
+		},
+		{
+			name: "Should return 32",
+			args: args{
+				[]int{6, 9, 3, 4, 5, 8},
+			},
+			want: 32,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxWaterContainerBruteForce(tt.args.heights); got != tt.want {
+				t.Errorf("maxWaterContainerBruteForce() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func Test_maxWaterContainer(t *testing.T) {
 	type args struct {
 		heights []int
@@ -12,18 +59,11 @@ func Test_maxWaterContainer(t *testing.T) {
 		want int
 	}{
 		{
-			name: "Should return correct result of 32",
+			name: "Should return correct result of 28",
 			args: args{
-				[]int{4, 8, 1, 2, 3, 9},
+				[]int{7, 1, 2, 3, 9},
 			},
-			want: 32,
-		},
-		{
-			name: "Should return 0",
-			args: args{
-				[]int{4},
-			},
-			want: 0,
+			want: 28,
 		},
 		{
 			name: "Should return 0 empty array",
@@ -33,11 +73,18 @@ func Test_maxWaterContainer(t *testing.T) {
 			want: 0,
 		},
 		{
-			name: "Should return 0 empty array",
+			name: "Should return 0 one el only",
 			args: args{
-				[]int{2, 4},
+				[]int{7},
 			},
-			want: 2,
+			want: 0,
+		},
+		{
+			name: "Should return correct result of 32",
+			args: args{
+				[]int{6, 9, 3, 4, 5, 8},
+			},
+			want: 32,
 		},
 	}
 	for _, tt := range tests {
